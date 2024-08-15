9f044878192b68e4d1eeb4f71fe443e24cfa426b
Alvin <alvinngg9@gmail.com>
2024-08-14 17:22:54 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,14 +5,21 @@ import java.util.HashMap;
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int result = 0;
-        int sum = 0;
+        //initialize the hashmap of key integer and value integer
         HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        //add the base case
         map.put(0, 1);
+        //iterate through the given array
         for (int num : nums) {
+            //find the sum up to the current num
             sum += num;
+            //check if the map has the key (sum - k)
             if (map.containsKey(sum - k)) {
-                result += map.get(sum -k);
+                //add the value to the result
+                result += map.get(sum - k);
             }
+            //add the current sum and its frequency as the value to the map
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return result;
